6c9d132677abbf58387ec080e27faaa5a3232ba2
Jesper Hills <jay@nopr.co>
2015-06-17 13:55:19 +0100

    Faction creation, modelling, routing start.

--- app/app.js
+++ app/app.js
@@ -4,11 +4,22 @@ var api = 'http://xwing-campaign-tracker.herokuapp.com';
 
 app.controller('main', ['$scope', '$http', function ($scope, $http) {
 
+  $scope.faction_form = null
   $scope.factions = []
 
-  $http.get(api+'/factions').success(function (data) {
-    console.log(data)
+  $http.get(api+'/faction').success(function (data) {
     $scope.factions = data;
   });
+
+  $scope.addFaction = function (a, b) {
+    var faction = {
+      slug: $scope.faction_slug,
+      name: $scope.faction_name
+    };
+    $http.post(api+'/faction/'+faction.slug, faction).success(function (data) {
+      console.log(data);
+      $scope.factions.push(data);
+    });
+  }
 
 }]);
