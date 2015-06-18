4f1cf6436ca3789257ef2870321a983194deeca1
Jesper Hills <jay@nopr.co>
2015-06-18 11:53:39 +0100

    Full angular routing, controller separation, views and player handling.

--- app/app.js
+++ app/app.js
@@ -1,24 +1,96 @@
 
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute']);
 var api = 'http://xwing-campaign-tracker.herokuapp.com';
 
-app.controller('main', ['$scope', '$http', function ($scope, $http) {
+// Configuration
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: '/app/view/main.html',
+      controller: 'MainController'
+    })
+    .when('/players', {
+      templateUrl: '/app/view/player.html',
+      controller: 'PlayerController'
+    })
+    .when('/players/:id', {
+      templateUrl: '/app/view/player.html',
+      controller: 'PlayerController'
+    })
+    .when('/factions', {
+      templateUrl: '/app/view/faction.html',
+      controller: 'FactionController'
+    })
+    .when('/factions/:slug', {
+      templateUrl: '/app/view/faction.html',
+      controller: 'FactionController'
+    });
+});
 
-  $scope.faction_form = null
-  $scope.factions = []
+// Directive: navigation
+app.directive('navigation', ['$http', function ($http) {
+  return {
+    templateUrl: '/app/view/navigation.html',
+    scope: '=',
+    link: function (scope, element) {
+      // Get the factions
+      $http.get(api+'/faction').success(function (data) {
+        scope.factions = data;
+      });
+      // Get the players
+      $http.get(api+'/player').success(function (data) {
+        scope.players = data;
+      });
+    }
+  }
+}]);
 
-  $http.get(api+'/faction').success(function (data) {
-    $scope.factions = data;
-  });
+// Controller: Main
+app.controller('MainController', ['$scope', '$http', function ($scope, $http) {
 
-  $scope.addFaction = function (a, b) {
-    var faction = {
-      slug: $scope.faction_slug,
-      name: $scope.faction_name
-    };
-    $http.post(api+'/faction/'+faction.slug, faction).success(function (data) {
-      console.log(data);
+  console.log('main controller')
+
+}]);
+
+// Controller: Faction
+app.controller('FactionController', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+
+  // Single faction, get data
+  if ($routeParams.slug) {
+    console.log('one faction')
+    $http.get(api+'/faction/'+$routeParams.slug).success(function (data) {
+      $scope.faction = data;
+    });
+  } else {
+    console.log('all factions')
+    console.log($scope.factions)
+  }
+
+  $scope.addplayer = function () {
+    $http.post(api+'/faction/', $scope.new_faction).success(function (data) {
       $scope.factions.push(data);
+    });
+  }
+
+}]);
+
+// Controller: Player
+app.controller('PlayerController', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+
+  // Single faction, get data
+  if ($routeParams.slug) {
+    console.log('one player')
+    $http.get(api+'/player/'+$routeParams.slug).success(function (data) {
+      $scope.player = data;
+    });
+  } else {
+    console.log('all players')
+    console.log($scope.players)
+  }
+
+  $scope.addplayer = function () {
+    $http.post(api+'/player/', $scope.new_player).success(function (data) {
+      $scope.players.push(data);
     });
   }
 
